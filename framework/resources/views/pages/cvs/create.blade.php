3557cb2906f5b5e4c801759178d6e503b4a75f45
Horia Mut <horia.mut@net2000.ch>
2015-12-12 22:13:10 +0100

    Update creation view.

--- framework/resources/views/pages/cvs/create.blade.php
+++ framework/resources/views/pages/cvs/create.blade.php
@@ -16,7 +16,7 @@
                 <div class='form-group' {!! $errors->has('name') ? 'has-error' : '' !!} >
                     {!! Form::label('name', 'CV name',['class' => 'sr-only']) !!}
                     {!! Form::text('name','My First CV',['required', 'class' => 'form-control', 'placeholder' => 'My First CV']) !!}
-                    {!! $errors->first('firstName', '<small class="help-block">:message</small>') !!}
+                    {!! $errors->first('name', '<small class="help-block">:message</small>') !!}
                 </div>
             </div>
         </div>
@@ -31,40 +31,40 @@
                         <label class='radio-inline'>
                             {!! Form::label('sex', 'M') !!}
                         </label>
-                        {!! Form::radio('sex', 'Mister', ['autofocus', 'required']) !!}
+                        {!! Form::radio('sex', 'm', ['autofocus', 'required']) !!}
 
                         <label class='radio-inline'>
                             {!! Form::label('sex', 'M\'am'/*,['class' => 'radio-inline']*/) !!}
                         </label>
-                        {!! Form::radio('sex', 'Miss') !!}
+                        {!! Form::radio('sex', 'f') !!}
 
                         {!! $errors->first('sex', '<small class="help-block">:message</small>') !!}
                     </div>
                 </div>
 
                 <div class="col-md-2 personal-details">
-                    <div class='form-group' {!! $errors->has('firstName') ? 'has-error' : '' !!} >
-                        {!! Form::label('firstName', 'Surname') !!}
-                        {!! Form::text('firstName','',['required', 'class' => 'form-control', 'placeholder' => 'Samuels']) !!}
-                        {!! $errors->first('firstName', '<small class="help-block">:message</small>') !!}
+                    <div class='form-group' {!! $errors->has('surname') ? 'has-error' : '' !!} >
+                        {!! Form::label('surname', 'Surname') !!}
+                        {!! Form::text('surname','',['required', 'class' => 'form-control', 'placeholder' => 'Samuels']) !!}
+                        {!! $errors->first('surname', '<small class="help-block">:message</small>') !!}
                     </div>
 
-                    <div class='form-group' {!! $errors->has('firstName') ? 'has-error' : '' !!} >
-                        {!! Form::label('nickName', 'Surname') !!}
-                        {!! Form::text('nickName','',['class' => 'form-control', 'placeholder' => $user->name]) !!}
-                        {!! $errors->first('firstName', '<small class="help-block">:message</small>') !!}
+                    <div class='form-group' {!! $errors->has('nickname') ? 'has-error' : '' !!} >
+                        {!! Form::label('nickname', 'Nickname') !!}
+                        {!! Form::text('nickname','',['class' => 'form-control', 'placeholder' => $user->name]) !!}
+                        {!! $errors->first('nickname', '<small class="help-block">:message</small>') !!}
                     </div>
 
-                    <div class='form-group' {!! $errors->has('lastName') ? 'has-error' : '' !!} >
-                        {!! Form::label('lastName', 'Lastname') !!}
-                        {!! Form::text('lastName','',['required', 'class' => 'form-control', 'placeholder' => 'Sighmore']) !!}
-                        {!! $errors->first('lastName', '<small class="help-block">:message</small>') !!}
+                    <div class='form-group' {!! $errors->has('lastname') ? 'has-error' : '' !!} >
+                        {!! Form::label('lastname', 'Lastname') !!}
+                        {!! Form::text('lastname','',['required', 'class' => 'form-control', 'placeholder' => 'Sighmore']) !!}
+                        {!! $errors->first('lastname', '<small class="help-block">:message</small>') !!}
                     </div>
 
-                    <div class='form-group' {!! $errors->has('birthDate') ? 'has-error' : '' !!} >
-                        {!! Form::label('birthDate', 'Date of birth') !!}
-                        {!! Form::date('birthDate','',['required', 'class' => 'form-control', 'placeholder' => 'dd.MM.year']) !!}
-                        {!! $errors->first('birthDate', '<small class="help-block">:message</small>') !!}
+                    <div class='form-group' {!! $errors->has('date_birth') ? 'has-error' : '' !!} >
+                        {!! Form::label('date_birth', 'Date of birth') !!}
+                        {!! Form::date('date_birth','',['required', 'class' => 'form-control', 'placeholder' => 'dd.MM.year']) !!}
+                        {!! $errors->first('date_birth', '<small class="help-block">:message</small>') !!}
                     </div>
                 </div>
 
@@ -103,7 +103,7 @@
                     <div class="personal-contact-digital">
                         <div class='form-group' {!! $errors->has('mail') ? 'has-error' : '' !!} >
                             {!! Form::label('mail', 'E-Mail') !!}
-                            {!! Form::email('mail','',['required', 'class' => 'form-control', 'placeholder' => 'roger.sighmore@hotmail.com']) !!}
+                            {!! Form::email('mail',$user->email,['required', 'class' => 'form-control', 'placeholder' => $user->email]) !!}
                             {!! $errors->first('mail', '<small class="help-block">:message</small>') !!}
                         </div>
 
@@ -136,10 +136,10 @@
                                 <div class="panel-title">Job Title</div>
                             </div>
                             <div class="panel-body">
-                                <div class='form-group' {!! $errors->has('function') ? 'has-error' : '' !!} >
-                                    {!! Form::label('function', 'Job Title',['class' => 'sr-only']) !!}
-                                    {!! Form::text('function','',['class' => 'form-control', 'placeholder' => 'Evil Mastermind']) !!}
-                                    {!! $errors->first('function', '<small class="help-block">:message</small>') !!}
+                                <div class='form-group' {!! $errors->has('job_name') ? 'has-error' : '' !!} >
+                                    {!! Form::label('job_name', 'Job Title',['class' => 'sr-only']) !!}
+                                    {!! Form::text('job_name','',['class' => 'form-control', 'placeholder' => 'Evil Mastermind']) !!}
+                                    {!! $errors->first('job_name', '<small class="help-block">:message</small>') !!}
                                 </div>
                             </div>
                         </div>
